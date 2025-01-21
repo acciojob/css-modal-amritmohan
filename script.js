72faf34e55450c4d8c0703edb90dd068624fc967
@amritmohan <amritmohan2284@gmail.com>
2025-01-21 23:35:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+document.getElementById('openModal').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'flex';
+});
+
+document.querySelector('.close-modal').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target === document.getElementById('modal')) {
+        document.getElementById('modal').style.display = 'none';
+    }
+});
+
